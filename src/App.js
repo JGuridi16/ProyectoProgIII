03061796825c8d4a86a42efeb2440b399986fa23
Gabriel Abreu <abreugabriel237@gmail.com>
2021-06-07 22:44:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/js/all.js';
+import './App.css';
+import Layout from './layout';
+
+const App = () => {
+  return <Layout />;
+};
+
+
+export default (App);
